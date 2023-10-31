5833a04496ccdba0d2dacd7db590897a2ff209b7
renesitoASC <frank.cl2810@gmail.com>
2023-10-31 13:55:30 -0300

    'asc'

---
+++ Practica cadenas/practicaestoyreloco.py
@@ -0,0 +1,9 @@
+chat = input('Ingrese un mensaje a MacheteVilches: ')
+nombre_usuario = 'renesitoo'
+confirmacion = chat.find('sugar')
+
+if confirmacion == 0:
+    print('sugar verga')
+else:
+    print(f'{nombre_usuario}: {chat} ')
+   
