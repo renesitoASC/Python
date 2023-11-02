d6b68f6345537bbf19f3969ed0ef1870256e4a78
renesitoASC <frank.cl2810@gmail.com>
2023-11-02 17:04:31 -0300

    Condicional04 hecho

--- Practica Condicionales/Condicional04.py
+++ Practica Condicionales/Condicional04.py
@@ -1,6 +1,20 @@
-sexo_ingresado = input("Ingrese aqui su sexo (hombre o mujer): ")
-aux_sexo = sexo_ingresado.lower().find('h')
+sexo_ingresado = input('Ingrese su sexo: ')
+aux_sexo = sexo_ingresado.lower()
 
 nombre_ingresado = input("Ingrese su nombre: ")
+aux_nombre = nombre_ingresado.lower()
 
-aux_nombre = nombre_ingresado.lower().find('m')
+#print(aux_nombre)       Auxiliares para verificar que los metodos se hayan aplicado y funcionen bien
+#print(aux_sexo)
+
+if aux_sexo.startswith('m'):
+    if aux_nombre < 'm':
+        print('Estas en el grupo A, mamita.')
+    else:
+        print('Estas en el grupo B, mamita.')
+
+if aux_sexo.startswith('h'):
+    if aux_nombre > 'n':
+        print('Estas en el grupo A, pibe')
+    else:
+        print('Estas en el grupo B, pibe.')
