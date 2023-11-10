3fd5b8148762a549d032d07b9e29bb5134eb0aa7
renesitoASC <frank.cl2810@gmail.com>
2023-11-10 17:08:10 -0300

    proyecto final terminado
    Signed-off-by: renesitoASC <frank.cl2810@gmail.com>

--- PROYECTO CON MODULOS/Proyeto_final.py
+++ PROYECTO CON MODULOS/Proyeto_final.py
@@ -1,3 +1,4 @@
+from operator import truediv
 import pprint
 import sys
 sys.path.append('c:\\Users\\frank\\OneDrive\\Escritorio\\Python\\Practica_modulos_calculos')
@@ -6,8 +7,8 @@ sys.path.append('c:\\Users\\frank\\OneDrive\\Escritorio\\Python\\Practica_modulo
 import Despedia 
 import Suma
 import Resta
-import Lenguaje_c
-import Lenguaje_python
+import Lenguaje_c as opcion_c
+import Lenguaje_python as opcion_py
 import Saludo_cordial as m_saludo_cordial
 import Saludo_normal as m_saludo_normal
 
@@ -20,10 +21,10 @@ while True:
     print('------------------')
     pregunta_al_usuario = input('Ingrese la opcion que desee realizar: ')
 
-    if len(pregunta_al_usuario) == 1:
+    if len(pregunta_al_usuario) == 1 and pregunta_al_usuario in ['1','2','3','4']:
         break                                       # Evaluando que ingrese solo UNA opcion.
     else:
-        print('Elija solo una opcion por favor.')
+        print('Elija una opcion valida por favor.')
 
 if pregunta_al_usuario == '1':
     while True:
@@ -34,10 +35,10 @@ if pregunta_al_usuario == '1':
         print('------------')
         decision_usuario_calculos = input('Ingrese el calculo que desa realizar: ')
 
-        if len(decision_usuario_calculos) == 1:
+        if len(decision_usuario_calculos) == 1 and decision_usuario_calculos in ['1','2']:
             break
         else:
-          print('Por favor elija solo un calculo a realizar.')
+          print('Por favor elija un calculo a realizar valido.')
 
     if decision_usuario_calculos == '1':
         print(Suma.Funcion_suma())          # Imprimiendo funciones para CALCULOS
@@ -52,13 +53,35 @@ if pregunta_al_usuario == '2':
         print('2- Saludo normal.')          # Menu para SALUDOS
         print('------------')
         decision_usuario_saludo = input('Ingrese el tipo de saludo que quiere recibir: ')
-        if len(decision_usuario_saludo) == 1:
+        if len(decision_usuario_saludo) == 1 and decision_usuario_saludo in ['1','2']:
             break
         else:
-            print('Ingrese un solo tipo de saludo.')
+            print('Ingrese un tipo de saludo valido por favor.')
 
     if decision_usuario_saludo == '1':
         print(m_saludo_cordial.Saludo_cordial())
     elif decision_usuario_saludo == '2':    # Imprimiendo funciones para SALUDOS
         print(m_saludo_normal.Saludo_normal())
 
+if pregunta_al_usuario == '3':
+    while True:
+        print('\nUsted ha elegido "Te digo que sos dependiendo que lenguaje estudies."')
+        print('------------')
+        print('1- Estudio lenguaje Python.')
+        print('2- Estudio lenguaje C.')     # Menu para LENGUAJES
+        print('------------')
+        decision_usuario_lenguaje = input('Ingrese el lenguaje que esta estudiando: ')
+        if len(decision_usuario_lenguaje) == 1 and decision_usuario_lenguaje in ['1','2']:
+            break
+        else:
+            print('Por favor ingrese solo un lenguaje valido (1 o 2).')
+        
+    if decision_usuario_lenguaje == '1':
+        print(opcion_py.Lenguaje_python())
+    elif decision_usuario_lenguaje == '2':  # Imprimiendo funciones para LENGUAJES
+        print(opcion_c.Lenguaje_c())
+        
+if pregunta_al_usuario == '4':
+        print('\nUsted ha elegido "Despido".')  # Opcion 4 que solo imprime la llamada al modulo y a su unica variable.
+        print(Despedia.variable_despedida)
+        
